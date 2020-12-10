a10eb22aa16b1738cc9b1e0af53016316fb0893e
rishavshekhar <66601671+rishavshekhar@users.noreply.github.com>
2020-12-10 12:09:34 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,40 @@
+import React from 'react';
+
+import { Cards, CountryPicker,} from './components';
+import { fetchData } from './api/';
+import styles from './App.module.css';
+
+import image from './images/image.png';
+
+class App extends React.Component {
+  state = {
+    data: {},
+    country: '',
+  }
+
+  async componentDidMount() {
+    const data = await fetchData();
+
+    this.setState({ data });
+  }
+
+  handleCountryChange = async (country) => {
+    const data = await fetchData(country);
+
+    this.setState({ data, country: country });
+  }
+
+  render() {
+    const { data} = this.state;
+
+    return (
+      <div className={styles.container}>
+        <img className={styles.image} src={image} alt="COVID-19" />
+        <CountryPicker handleCountryChange={this.handleCountryChange} />
+        <Cards data={data} />
+      </div>
+    );
+  }
+}
+
+export default App;
